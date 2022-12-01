9b1250d723daaf7413876b98526c13aac9f9526e
marioamatucci <mario@goodfit.io>
2022-12-01 11:31:22 +0100

    wip

---
+++ macros/e2e_test.sql
@@ -0,0 +1,25 @@
+{% macro e2e_test(table_name) %} 
+WITH test AS ( 
+    {% set expected_cases %}
+    select 
+    * 
+from {{ ref('expected_'+table_name)}}
+    {% endset %}
+
+    {% set actual_cases %}
+        select 
+    * 
+from {{ ref(table_name)}}
+    {% endset %}
+
+    {{ audit_helper.compare_queries(
+        a_query=expected_cases,
+        b_query=actual_cases
+    ) }}
+)
+/*  when expected_cases and actual_cases differ anyhow, test fails */
+SELECT *
+FROM test 
+WHERE in_a = FALSE OR in_b=FALSE 
+
+{% endmacro %}
--- models/gf_clean/apps/apps_latest.sql
+++ models/gf_clean/apps/apps_latest.sql
@@ -1,6 +1,15 @@
 {{ config(materialized="view", schema="clean") }}
 
+with cte as (
 
 select 'a'  as id , 123 as num
 union all
 select 'b' , NULL
+)
+
+
+select *
+from cte
+union  all
+select *
+FROM {{ source('gf_raw', 'apps')}}
